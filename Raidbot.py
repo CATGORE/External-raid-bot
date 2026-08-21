1386bb5fd9ab260729340af7f3347796734e731c
CG <anonymous93176@gmail.com>
2026-08-21 08:46:09 +0300

    Refactor spam command to use Nagger class

--- Raidbot.py
+++ Raidbot.py
@@ -26,13 +26,22 @@ async def say(interaction: discord.Interaction, message: str, ghost: bool = Fals
     if ghost:
         await FUCK_YOU_YXORD.delete()
 
+class Nagger(discord.ui.View):
+    def __init__(self, message: str):
+        super().__init__()
+        self.message = message
+
+    @discord.ui.button(label="spam", style=discord.ButtonStyle.danger)
+    async def fl00d(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(".", ephemeral=True)
+        for _ in range(5):
+            await interaction.followup.send(self.message, ephemeral=False)
+
+
 @client.tree.command(name="spam")
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-async def fl00d(interaction: discord.Interaction, message: str):
-    await interaction.response.send_message(".", ephemeral=True)
-    for _ in range(5):
-        await interaction.followup.send(message, ephemeral=False)
-
+async def spam(interaction: discord.Interaction, message: str):
+    await interaction.response.send_message(view=Nagger(message))
 
 client.run("")
